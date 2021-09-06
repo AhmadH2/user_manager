64a01b57fef93b1c2a325ccca13d2ff3c7bce2f6
AhmadH2 <62361558+AhmadH2@users.noreply.github.com>
2021-09-06 15:51:14 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAnPQj2R3TdAqRGG3KMBoarMFDR0RrrEt0",
+    authDomain: "usersmanager-62455.firebaseapp.com",
+    projectId: "usersmanager-62455",
+    storageBucket: "usersmanager-62455.appspot.com",
+    messagingSenderId: "406333973033",
+    appId: "1:406333973033:web:f698e2e8767baea3fe7238",
+    measurementId: "G-S6JQPZWR7D"
+  }
 };
